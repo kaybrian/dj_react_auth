0809d743d67dfb5283a56d1a4fb0e91009cf5528
kaybrian <kayonbrian@gmail.com>
2023-02-08 11:21:14 +0200

    pawword erors

--- backend/users/serilaizers.py
+++ backend/users/serilaizers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 
 User = get_user_model()
 
@@ -7,6 +9,22 @@ class UserCreateSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name','last_name','email','password']
+
+    def  validate(self, data):
+        password = data.get('password')
+        user = User(**data)
+
+        try:
+            validate_password(password, user)
+        except exceptions.ValidationError as e:
+            serializer_errors = serializers.as_serializer_error(e)
+            raise exceptions.ValidationError(
+                {
+                    "password":serializer_errors['non_field_errors']
+                }
+            )
+
+
 
     def create(self, validated_data):
         user = User.objects.create_user(
