43d7ee0ae661c3fc23db275459f102fd31c6dd57
kaybrian <kayonbrian@gmail.com>
2023-02-10 00:14:59 +0200

    react url done

--- frontend/client/src/components/Layout.js
+++ frontend/client/src/components/Layout.js
@@ -1,19 +1,22 @@
 import Navbar from "./Navbar"
 import { Helmet } from "react-helmet";
 
-const Layout = ({ title, content, Children }) => {
-    <>
-        <Helmet>
-            <title>{title}</title>
-            <meta name="description" content={content} />
-        </Helmet>
+const Layout = ({ title, content, children }) => {
+    return (
+        <>
+            <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={content} data-react-helmet="true" />
+            </Helmet>
 
-        <Navbar />
-        <div className="container mt-5">
-            {Children}
-        </div>
-    </>
+            <Navbar />
+            <div className="container mt-5">
+                {children}
+            </div>
+        </>
+    )
 };
 
 
 export default Layout;
+
--- frontend/client/src/containers/Home.js
+++ frontend/client/src/containers/Home.js
@@ -2,7 +2,7 @@ import Layout from "components/Layout";
 
 const HomePage = () => {
     return (
-        <Layout title="Auth site | Home" content="home page">
+        <Layout title="Auth site | Home page" content="Welocme to our home page">
             <h1>Home</h1>
         </Layout>
     )
