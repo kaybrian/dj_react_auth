c1f5fdc4e571d86088750e73ed6c3c11f231f228
kaybrian <kayonbrian@gmail.com>
2023-02-10 11:43:03 +0200

    alittle time to think

--- frontend/client/src/containers/Register.js
+++ frontend/client/src/containers/Register.js
@@ -1,9 +1,99 @@
 import Layout from 'components/Layout';
+import { useSelector, useDispatch } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import { register } from 'features/user';
+
 
 const Register = () => {
+  const { registered, loading } = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+  })
+
+  const onChange = e => setFormData({
+    ...formData,
+    [e.target.name]: e.target.value
+  });
+
+  const { first_name, last_name, email, password } = formData;
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    dispatch(register({ first_name, last_name, email, password }));
+
+  };
+
+  if (registered) {
+    return <Navigate to='/login' />;
+  };
+
   return (
     <Layout title='Auth site | Register User' content='Register for the platform'>
-      <h1>Register</h1>
+      <h1>Register for an account</h1>
+      <form className='mt-5'  >
+        <div className='form-group'>
+          <label htmlFor='first_name' className='form-label'>First name </label>
+          <input
+            className='form-control'
+            type='text'
+            name='first_name'
+            onChange={onChange}
+            value={first_name}
+            required
+          />
+        </div>
+        <div className='form-group mt3'>
+          <label htmlFor='last_name' className='form-label'>last name </label>
+          <input
+            className='form-control'
+            type='text'
+            name='last_name'
+            onChange={onChange}
+            value={last_name}
+            required
+          />
+        </div>
+        <div className='form-group mt3'>
+          <label htmlFor='email' className='form-label'>Email </label>
+          <input
+            className='form-control'
+            type='email'
+            name='email'
+            onChange={onChange}
+            value={email}
+            required
+          />
+        </div>
+        <div className='form-group mt3'>
+          <label htmlFor='password' className='form-label'>Password </label>
+          <input
+            className='form-control'
+            type='password'
+            name='password'
+            onChange={onChange}
+            value={password}
+            required
+          />
+        </div>
+        {
+        loading ? (
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        )
+          :
+          <button type='submit' onSubmit={onSubmit} className='btn btn-primary mt-4'>
+            Register
+          </button>
+        }
+      </form>
     </Layout>
   );
 };
--- frontend/client/src/features/user.js
+++ frontend/client/src/features/user.js
@@ -1,6 +1,38 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { API_URL } from 'config';
 
+
+export const register = createAsyncThunk(
+  'users/register',
+  async ({ first_name, last_name, email, password }, thunkAPI) => {
+
+    const body = JSON.stringify({
+      first_name,
+      last_name,
+      email,
+      password
+    })
+    try {
+      const res = await fetch(`/api/users/register`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body
+      })
+
+      const data = await res.json();
+
+      if (res.status === 201) {
+        return data;
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
 const initialState = {
   isAuthenticated: false,
   user: null,
@@ -15,39 +47,24 @@ const userSlice = createSlice({
     restRegistered: state => {
       state.registered = false;
     }
+  },
+  extraReducers: builder => {
+    builder.addCase(register.pending, (state, action) => {
+      return (
+        state.loading = true
+      )
+    })
+    .addCase(register.fulfilled, (state, action) => {
+      state.loading = false;
+      state.registered = true;
+    })
+    .addCase(register.rejected, (state, action) => {
+      state.loading = false;
+
+    })
   }
 });
 
 export const { restRegistered } = userSlice.actions;
 export default userSlice.reducer;
 
-const register = createAsyncThunk(
-  'users/register/',
-  async ({ first_name, last_name, email, password }, thunkAPI) => {
-
-    const body = JSON.stringify({
-        first_name,
-        last_name,
-        email,
-        password
-    })
-    try{
-        const res = await fetch(`${API_URL}/api/users/register`,{
-            method: 'POST',
-            headers:{
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body
-        })
-
-        if (res.status === 201){
-            return data;
-        }else{
-            return thunkAPI.rejectWithValue(data);
-        }
-    } catch (err){
-        return thunkAPI.rejectWithValue(err.response.data);
-    }
-  }
-);
